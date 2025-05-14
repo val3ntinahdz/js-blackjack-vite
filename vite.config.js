b10eb392fb65252e97e01a2709f7d2c183cfa9a8
val3ntinahdz <valentinaleonh8@gmail.com>
2025-05-13 22:35:21 -0600

    Configure base environment from Vite

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from "vite";
+
+
+export default defineConfig({ 
+    base: '/js-blackjack-vite/', // Base public path when served in development or production. Check vite's documentation for more info: https://vite.dev/config/shared-options.html#base
+    plugins: [] // Array of plugins to use. To see more about this shared option, check: https://vite.dev/config/shared-options.html#plugins
+
+});
